f23d291c610d014904f1d9e46436c05536caae90
julwrites <julian.teh@outlook.com>
2020-04-26 14:42:01 +0800

    Fixing routing for blog

--- src/router.ts
+++ src/router.ts
@@ -4,18 +4,18 @@ import Home from './views/Home.vue';
 
 Vue.use(Router);
 
-import BlogEntries from './content/blog/posts.json';
+import BlogEntries from './content/blog/contents.json';
 
 const blogRoutes = Object.keys(BlogEntries).map(section => {
-    const children = BlogEntries[section].map(child => ({
-        path: child.id,
-        name: child.id,
-        component: () => import(`./content/blog/${section}/${child.id}.md`)
+    const children = BlogEntries.blog.map(entry => ({
+        path: entry.id,
+        name: entry.id,
+        component: () => import(`./content/blog/${section}/${entry.id}.md`)
     }));
     return {
         path: `/${section}`,
         name: section,
-        component: () => import(`./views.Blog.vue`),
+        component: () => import(`./views/Blog.vue`),
         children
     };
 });
