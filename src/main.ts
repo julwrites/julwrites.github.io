195698ec13456700e2547104a7bfc019ce673f3f
julwrites <julian.teh@outlook.com>
2020-06-14 12:23:04 +0800

    Adding style update

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import Vue from 'vue';
+import VueDarkMode from '@growthbunker/vuedarkmode'
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+Vue.use(VueDarkMode);
 
 new Vue({
   router,
