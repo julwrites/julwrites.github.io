b904e7975937541e90b5e8d650ef5cda87a9c5da
julwrites <julian.teh@outlook.com>
2021-11-14 23:45:48 +0800

    Updating to use elm-ui as the framework for the front-end

--- src/Main.elm
+++ src/Main.elm
@@ -1,26 +1,55 @@
 module Main exposing (..)
 
-import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Element exposing (Element, alignRight, centerY, el, fill, padding, rgb255, row, spacing, text, width)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
 
 
 
--- MODEL
-
-
-type alias Model =
-    Int
-
-
-init : Model
-init =
-    0
-
-
-
--- MAIN
+-- main is a the 'monolithic' model describing this application
+-- Note: main must be of type Html, Svg or Programs
 
 
 main =
-    Browser.sandbox { init = init, update = update, view = view }
+    Element.layout
+        -- The list of attributes
+        []
+        -- The layout's element; remember, elm-ui adopts a 1 child policy
+        -- Elements of the resulting model
+        landing
+
+
+landing : Element msg
+landing =
+    Element.column []
+        [ menu
+        , blurb
+        , footer
+        ]
+
+
+menu : Element msg
+menu =
+    Element.row
+        []
+        [ Element.el [] (Element.text "About"), Element.text "Projects", Element.text "Blog" ]
+
+
+blurb : Element msg
+blurb =
+    Element.column
+        []
+        [ Element.el [] (text "Hi, I'm Julian")
+        , Element.el [] (text "Welcome to my domain")
+        ]
+
+
+footer : Element msg
+footer =
+    Element.row
+        []
+        [ Element.el [] (text "Github")
+        , Element.el [] (text "LinkedIn")
+        , Element.el [] (text "Email")
+        ]
