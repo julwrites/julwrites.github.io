118dbb29dcf1297d4202f009c714a34a58bcd8f0
julwrites <mail@tehj.io>
2025-04-14 00:22:15 +0800

    More cleanup

--- src/blog/src/consts.ts
+++
@@ -1,8 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Julwrites Stuff';
-export const SITE_DESCRIPTION = 'Welcome to my log';
-export const SITE_BASEPATH = "/blog";
-export const SITE_COLOR_MODE = "dark";
-export const SITE_THEME = "prose dark:bg-neutral-800 dark:prose-invert prose-headings:text-primary-300 visited:prose-a:text-secondary-400";
