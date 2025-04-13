b2b198fa4d99d8e5294d58c1dd4f9b17cf3b553f
julwrites <mail@tehj.io>
2025-04-13 23:56:29 +0800

    Updating the blog page linkage

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,23 +1,19 @@
 import { defineCollection, z } from 'astro:content';
 
+const blogSchema = z.object({
+  title: z.string(),
+  description: z.string(),
+  pubDate: z.coerce.date(),
+  updatedDate: z.coerce.date().optional(),
+  heroImage: z.string().optional(),
+});
+
 const blog = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+  schema: blogSchema,
 });
 
 const projects = defineCollection({
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
+  schema: blogSchema,
 });
 
 export const collections = { blog, projects };
