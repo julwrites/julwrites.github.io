b8114a6e4724964c982cb6ae8366cc742ca3debb
julwrites <mail@tehj.io>
2025-05-24 22:47:36 +0800

    Adding astro-umami

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,29 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 
+import yeskunallumami from "@yeskunall/astro-umami";
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://tehj.io',
-  integrations: [mdx(), sitemap(), vue(), tailwind()],
+  site: "https://tehj.io",
+  integrations: [
+    mdx(),
+    sitemap(),
+    vue(),
+    tailwind(),
+    yeskunallumami({ id: "73173525-4cfd-4b92-a118-04a8135c2c82" }),
+  ],
   vite: {
     optimizeDeps: {
-      exclude: ['fsevents']
+      exclude: ["fsevents"],
     },
     build: {
       rollupOptions: {
-        external: ['fsevents']
-      }
-    }
-  }
+        external: ["fsevents"],
+      },
+    },
+  },
 });
